6d69a0c2bd999e2d519f6b99ee0d53b458444014
ibarralucas97 <ibarralucas@outlook.es>
2025-11-05 17:21:12 -0300

    Reagregado firebase-config.js para funcionamiento en producción

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// 🔥 Configuración Firebase — versión funcional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDg3k7c4j7MMpyModbS63byuo9JmhuAD0c",
+  authDomain: "carniceria-lucas.firebaseapp.com",
+  projectId: "carniceria-lucas",
+  storageBucket: "carniceria-lucas.appspot.com",
+  messagingSenderId: "285806072223",
+  appId: "1:285806072223:web:7dcde9d3b7e1f2b1bf6daa"
+};
+
