4ebb0005643e916645d32a834c822c0aefa583b1
ibarralucas97 <ibarralucas@outlook.es>
2025-11-10 16:04:38 -0300

    Arrelos v12

--- admin-token.js
+++ admin-token.js
@@ -1,8 +1,8 @@
 // admin-token.js
 // Placeholder para producción (no contiene tu clave real)
 // admin-token.js
-export const ADMIN_TOKEN = "FAKE_PUBLIC_TOKEN_2025";
+// export const ADMIN_TOKEN = "FAKE_PUBLIC_TOKEN_2025";
 
 // admin-token.js (solo en tu PC)
-// export const ADMIN_TOKEN = "xLucasSecureKey_2025";
+export const ADMIN_TOKEN = "xLucasSecureKey_2025";
 
