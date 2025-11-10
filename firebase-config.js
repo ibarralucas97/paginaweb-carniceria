258679097b56d1f2debcbeb5899a0d30e0249d14
ibarralucas97 <ibarralucas@outlook.es>
2025-11-10 15:03:51 -0300

    Arrelos v10

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDg3k7c4j7MMpyModbS63byuo9JmhuAD0c",
   authDomain: "carniceria-lucas.firebaseapp.com",
   projectId: "carniceria-lucas",
-  storageBucket: "carniceria-lucas.appspot.com",
+  storageBucket: "carniceria-lucas.firebasestorage.app",
   messagingSenderId: "285806072223",
   appId: "1:285806072223:web:7dcde9d3b7e1f2b1bf6daa"
 };
